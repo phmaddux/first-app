d0b847899cc3325d9c8e363be24fd53387a52f7a
Hunter Maddux <phmaddux@gmail.com>
2017-09-26 17:38:11 -0400

    mongo and heroku set up

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -21,6 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use('/', index);
 app.use('/users', users);
